0f22f2b912ea1058bb47c2935f817e30a375eac4
Huclark <vhuclark@gmail.com>
2024-01-10 15:42:58 +0000

    Update: added new classes effect

--- console.py
+++ console.py
@@ -6,7 +6,12 @@
 import cmd
 import shlex
 import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
 
@@ -18,7 +23,12 @@ class HBNBCommand(cmd.Cmd):
 
     # Create all available classes
     __all_classes = {
+        "Amenity" : Amenity,
         "BaseModel" : BaseModel,
+        "City" : City,
+        "Place" : Place,
+        "Review" : Review,
+        "State" : State,
         "User" : User,
     }
 
