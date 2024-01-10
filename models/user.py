5a10ac51d84839b84d65b7c4716066ab1e5a8303
Huclark <vhuclark@gmail.com>
2024-01-10 11:37:59 +0000

    Add: User class

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""User class, a child of the BaseModel class
+"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Handles user information and inherits from the
+    BaseModel class
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
