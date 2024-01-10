9044af76393f484d450130436c7579cf04948fc0
Huclark <vhuclark@gmail.com>
2024-01-10 15:14:05 +0000

    Update: Cleaner code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,9 +12,15 @@ class FileStorage:
     and deserializes JSON files to instances
     """
     __file_path = "file.json"
+
+    # Eg: __objects = {class.id : "address of BaseModel Instance"}
     __objects = {}
 
-    # __objects = {class.id : "address of BaseModel Instance"}
+     # Create all available classes
+    __all_classes = {
+        "BaseModel" : BaseModel,
+        "User" : User,
+    }
 
     def all(self):
         """Returns the dictionary `__objects`
@@ -54,36 +60,6 @@ class FileStorage:
                 load_obj = json.load(file)
                 for key, value in load_obj.items():
                     class_name, obj_id = key.split(".")
-                    self.__objects[key] = globals()[class_name](**value)
+                    self.__objects[key] = self.__all_classes[class_name](**value)
         except FileNotFoundError:
             pass
-
-    def all_classes(self):
-        """Returns a dictionary of all available classes
-
-        Returns:
-            dict: Dictionary of all available classes
-        """
-        return {
-            "BaseModel" : BaseModel,
-            "User" : User
-        }
-
-    def create_instance(self, class_name):
-        """Creates a new instance of the specified class and
-        adds it to the dictionary.
-
-        Args:
-            class_name (str): The class name
-
-        Returns:
-            class: A new instance of the class
-        """
-        # create new instance of the class
-        new_instance = globals()[class_name]()
-        # Construct key
-        key = "{}.{}".format(class_name, new_instance.id)
-        # Assign Value to key
-        self.__objects[key] = new_instance
-
-        return new_instance
