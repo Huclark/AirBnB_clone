81ffa4d150db7242df6d9bafbc57f52daaddc6cd
Huclark <vhuclark@gmail.com>
2024-01-13 10:16:24 +0000

    Update: test new method

--- tests/test_engines/test_file_storage.py
+++ tests/test_engines/test_file_storage.py
@@ -45,11 +45,41 @@ class TestFileStorage(unittest.TestCase):
     def test_all(self):
         """Test the all method for the FileStorage class
         """
+        # Test if all() returns an empty dictionary
         self.assertEqual(self.test_storage.all(), {})
+        # Test if all() return value is of the type dict
         self.assertIsInstance(self.test_storage.all(), dict)
 
-
-
+    def test_new(self):
+        """Test the new method for the FileStorage class to check
+        if it adds new instances of various classes to the dictionary
+        """
+        # Create new instances for the classes
+        amenity = Amenity()
+        basemodel = BaseModel()
+        city = City()
+        place = Place()
+        review = Review()
+        state = State()
+        user = User()
+        # Call the new method on each instance
+        self.test_storage.new(amenity)
+        self.test_storage.new(basemodel)
+        self.test_storage.new(city)
+        self.test_storage.new(place)
+        self.test_storage.new(review)
+        self.test_storage.new(state)
+        self.test_storage.new(user)
+        # Retrieve the new dictionary created
+        object_dict = self.test_storage.all()
+        # Check if the instances are in the dictionary
+        self.assertIn("Amenity." + amenity.id, object_dict)
+        self.assertIn("BaseModel." + basemodel.id, object_dict)
+        self.assertIn("City." + city.id, object_dict)
+        self.assertIn("Place." + place.id, object_dict)
+        self.assertIn("Review." + review.id, object_dict)
+        self.assertIn("State." + state.id, object_dict)
+        self.assertIn("User." + user.id, object_dict)
 
 
 if __name__ == "__main__":
