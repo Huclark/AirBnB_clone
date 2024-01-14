e7dd531b3f8d0f58bf09427c36fafa941227944a
Huclark <vhuclark@gmail.com>
2024-01-14 11:27:10 +0000

    Update: Test empty command

--- tests/test_console.py
+++ tests/test_console.py
@@ -24,11 +24,11 @@ class TestHBNBCommand(unittest.TestCase):
         """Set up resources and  configurations needed for the
         test cases
         """
-        # Create an instance of the HBNBCommand class
-        self.console = HBNBCommand()
         # Create a mock stdin and stdout
         self.mock_stdin = create_autospec(sys.stdin)
         self.mock_stdout = create_autospec(sys.stdout)
+        # Create an instance of the HBNBCommand class
+        self.console = HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
         # Redirect sys.stdout to StringIO()
         self.out = StringIO()
         sys.stdout = self.out
@@ -59,12 +59,25 @@ class TestHBNBCommand(unittest.TestCase):
         # Set tje position of the StringIO object to the beginning
         self.out.seek(0)
 
-    def test_do_create(self, mock_stdout, mock_stdin):
-        """Test the do_Create() method
+    # def _last_write(self, nr=None):
+    #     """:return: last `n` output lines"""
+    #     if nr is None:
+    #         return self.mock_stdout.write.call_args[0][0]
+    #     return "".join(map(lambda c: c[0][0],
+    #                        self.mock_stdout.write.call_args_list[-nr:]))
 
-        Args:
-            mock_stdout (StringIO): Captures and inspect printed output
+    def test_enter_with_noinput(self):
+        """Tests for an empty command (empty string or newline character)
+
+        Note: onecmd() returns True for successful handling and False otherwise
+              In this case, an empty command should not be handled successfully,
+              so the expectation is False
         """
+        # Simulate pressing the enter key with no input
+        self.assertFalse(self.console.onecmd("\n"))
+        # Check if the standard output is an empty string
+        self.assertEqual(self.out.getvalue(), "")
+        
 
 
 if __name__ == "__main__":
