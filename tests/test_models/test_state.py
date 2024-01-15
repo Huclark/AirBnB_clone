a14e4113dbf960afbf92cee89fdddd8989ad81a8
Huclark <vhuclark@gmail.com>
2024-01-15 01:27:23 +0000

    Add: Test User

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -10,70 +10,66 @@ from models.state import State
 
 
 class TestState(unittest.TestCase):
-    """state class test cases
+    """State class test cases
 
     Args:
         unittest (module): Module for unit tests
     """
-    @classmethod
-    def setUp(cls) -> None:
+    def setUp(self):
         """set up class instance to use for test
         """
-        cls.instance = State()
-        
-    @classmethod
-    def tearDownClass(cls) -> None:
+        self.instance = State()
+
+    def tearDown(self):
         """Destroy instance of class state
         """
-        del cls.instance
-    
-     
-    def test_Amenity_attribute(self):
+        del self.instance
+
+    def test_state_attribute(self):
         """
-        test if the instance.id of type str
-        test if the instance.name of type string
+        Test State class attributes
         """
         self.assertIsInstance(self.instance.id, str)
         self.assertIsInstance(self.instance.name, str)
         # check updated_at and created_at are datetime obj
         self.assertIsInstance(self.instance.updated_at, datetime)
         self.assertIsInstance(self.instance.created_at, datetime)
-        # check if base_model is an instance of BaseModel
+        # check if state is an instance of State
         self.assertIsInstance(self.instance, State)
-        # check if base_model is a valid object __class__
+        # check if state is a valid object __class__
         self.assertTrue(hasattr(self.instance, "__class__"))
         # check if calling new() was successful
         self.assertIn(self.instance, models.storage.all().values())
 
     def test_amenity_kwargs(self):
-        """Test initialization of BaseModel with kwargs
+        """Test initialization of State with kwargs
         """
         object_data = {
             "id": "1234-1234-1234-123a",
             "created_at": datetime.isoformat(datetime.now()),
             "updated_at": datetime.isoformat(datetime.now()),
-            "__class__": "BaseModel",
+            "__class__": "State",
             "Country": "Ghana"
         }
-        base_model = State(**object_data)
-        # check if base_model is a valid object __class__
-        self.assertTrue(hasattr(base_model, "__class__"))
+        state = State(**object_data)
+        # check if state is a valid object __class__
+        self.assertTrue(hasattr(state, "__class__"))
         # Validate object id
-        self.assertEqual(object_data["id"], base_model.id)
+        self.assertEqual(object_data["id"], state.id)
         # Validate created_at and updated_at
         created_at = datetime.fromisoformat(object_data["created_at"])
         updated_at = datetime.fromisoformat(object_data["updated_at"])
-        self.assertEqual(created_at, base_model.created_at)
-        self.assertEqual(updated_at, base_model.updated_at)
+        self.assertEqual(created_at, state.created_at)
+        self.assertEqual(updated_at, state.updated_at)
         # Validate additional attributes)
-        self.assertEqual(object_data["Country"], base_model.Country)
-        
+        self.assertEqual(object_data["Country"], state.Country)
+
     def test_unused_args(self) -> None:
-       """
-       check if args is unused
-       """ 
-       self.assertNotEqual(None, self.instance.__dict__.values())
-       
+        """
+        check if args is unused
+        """
+        self.assertNotEqual(None, self.instance.__dict__.values())
+
     def test_created_time(self) -> None:
         """
         check if created at used datetime class to generate
@@ -81,14 +77,14 @@ class TestState(unittest.TestCase):
         """
         instance2 = State()
         self.assertLess(self.instance.created_at, instance2.created_at)
-        
+
     def test_updated_time(self) -> None:
         """
         check if updated time of two instance created at different time
         """
         instance2 = State()
         self.assertLess(self.instance.updated_at, instance2.updated_at)
-        
+
     def test_new_attribute_exist(self):
         """if name attribute exist
         """
@@ -104,12 +100,12 @@ class TestState(unittest.TestCase):
         instance2.save()
         self.assertNotEqual(updated_time, instance2.updated_at)
         self.assertLess(updated_time, instance2.updated_at)
-    
+
     def test_str(self) -> None:
         """test if str representation is overide"""
         dict_str = f"[State] ({self.instance.id}) {self.instance.__dict__}"
         self.assertEqual(dict_str, str(self.instance))
-        
+
     def test_to_method(self) -> None:
         """test to_dict method
         """
@@ -117,15 +113,14 @@ class TestState(unittest.TestCase):
         self.assertEqual(obj_dict["__class__"], "State")
         self.assertIsInstance(obj_dict["created_at"], str)
         self.assertIsInstance(obj_dict["updated_at"], str)
-    
+
     def test_updated_file(self) -> None:
         """test if the file is updated
         """
         self.instance.save()
-        with open("file.json", "r") as f:
+        with open("file.json", "r", encoding="utf-8") as f:
             self.assertIn("State." + self.instance.id, f.read())
-            
 
 
-if "__name__" == "__main__":
-     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
